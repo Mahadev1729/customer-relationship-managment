c626ef0364e7a3a2a4b2097aeb807b24afa4de2d
Mahadev1729 <devcampus17@gmail.com>
2025-08-13 19:28:48 +0530

    crm start

---
+++ mydb.py
@@ -0,0 +1,21 @@
+﻿
+
+import mysql.connector 
+
+
+dataBase=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='admin',
+    
+)
+
+## prepare cursor
+
+cursorObject=dataBase.cursor()
+
+## create database
+
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("ALL DONE")
